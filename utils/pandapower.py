6a180d3fd013cacc47daec250065073ae4432fd6
yamizi <salah.ghamizi@gmail.com>
2023-10-30 13:18:09 +0100

    Added feature scaler

--- utils/pandapower.py
+++ utils/pandapower.py
@@ -5,9 +5,11 @@ from torch_geometric.data import (
 )
 from pandapower.auxiliary import pandapowerNet
 import pandapower as pp
+import numpy as np
 import torch
 import pandas as pd
 from copy import deepcopy
+from sklearn.preprocessing import StandardScaler
 
 def build_costs(net, costs):
      #net.gen["cost"] = 0
@@ -30,12 +32,19 @@ class PandaPowerDataset(InMemoryDataset):
         assert self.preprocess in [None, 'metapath2vec', 'transe']
         super().__init__(None, transform, pre_transform)
 
-        hetero_data, edges, dataframes= build_hetero_data(network, include_res, opf_as_y)
+        hetero_data, edges, dataframes, scalers= build_hetero_data(network, include_res, opf_as_y)
         self.data, self.slices = hetero_data, None
+        self.scalers = scalers
+        self.dataframes = dataframes
+        self.edges = edges
 
     @property
     def num_outputs(self) -> int:
-        return self._data["ext_grid"].y.shape[0]
+        return 2 # np.sum([len(self._data[e].y.flatten()) for e in self.output_nodes if hasattr(self._data[e],"y")])
+    
+    @property
+    def output_nodes(self) -> [str]:
+        return [e for e in ["ext_grid","sgen","gen"] if hasattr(self._data[e],"y")]
 
 def build_torch_dataset(network):
     hetero_data, edges, dataframes= build_hetero_data(network)
@@ -46,6 +55,7 @@ def build_hetero_data(network, include_res=True, opf_as_y=True):
     data = HeteroData()
     edges = {}
     dataframes = {}
+    scalers = {}
     for node in node_types:
         edges_ = []
         edges_from = []
@@ -53,7 +63,8 @@ def build_hetero_data(network, include_res=True, opf_as_y=True):
         edges_to2 = []
 
         merged_df = deepcopy(getattr(network,node)) if (len(getattr(network,"res_"+node))==0 or not include_res) else pd.merge(getattr(network,node),getattr(network,"res_"+node),"left",on=None,left_index=True,right_index=True)
-        
+        if len(merged_df)==0:
+            continue
         if opf_as_y and node in ["gen","sgen","ext_grid"] and len(getattr(network,"res_"+node))>0:
             y = ["p_mw","q_mvar"]
             if include_res:
@@ -65,37 +76,39 @@ def build_hetero_data(network, include_res=True, opf_as_y=True):
             pf = getattr(network,"res_"+node)[y]
             data[node].y =torch.Tensor(pf.values.tolist())
 
-        merged_df.drop(columns=["name"],inplace=True)    
-        data[node].x =torch.Tensor(pd.get_dummies(merged_df).values.tolist())
+        merged_df.drop(columns=["name"],inplace=True)   
+        scaler = StandardScaler()
+        data[node].x =torch.Tensor(scaler.fit_transform(pd.get_dummies(merged_df).dropna(axis=1)))
+        scalers[node] = scaler
 
         if "from_bus" in merged_df.columns:
             edges_from = merged_df["from_bus"].tolist()
             merged_df.drop(columns=["from_bus"],inplace=True)
-            data['bus','to',node].edge_index = torch.Tensor([edges_from, merged_df.index.tolist()]).transpose(0,1)
+            data['bus','to',node].edge_index = torch.LongTensor([edges_from, merged_df.index.tolist()])
  
         if "to_bus" in merged_df.columns:
             edges_to = merged_df["to_bus"].tolist()
             merged_df.drop(columns=["to_bus"],inplace=True)
-            data[node,'to','bus'].edge_index = torch.Tensor([merged_df.index.tolist(),edges_to]).transpose(0,1)
+            data[node,'to','bus'].edge_index = torch.LongTensor([merged_df.index.tolist(),edges_to])
 
         if "hv_bus" in merged_df.columns:
             edges_from = merged_df["hv_bus"].tolist()
             merged_df.drop(columns=["hv_bus"],inplace=True)
-            data['bus','to',node].edge_index = torch.Tensor([edges_from, merged_df.index.tolist()]).transpose(0,1)
+            data['bus','to',node].edge_index = torch.LongTensor([edges_from, merged_df.index.tolist()])
 
             edges_to = merged_df["lv_bus"].tolist()
             merged_df.drop(columns=["lv_bus"],inplace=True)
-            data[node,'to','bus'].edge_index = torch.Tensor([merged_df.index.tolist(),edges_to]).transpose(0,1)
+            data[node,'to','bus'].edge_index = torch.LongTensor([merged_df.index.tolist(),edges_to])
 
         if "mv_bus" in merged_df.columns:
             edges_to2 = merged_df["mv_bus"].tolist()
             merged_df.drop(columns=["mv_bus"],inplace=True)
-            data[node,'to','bus'].edge_index = torch.Tensor([merged_df.index.tolist()+merged_df.index.tolist(),edges_to+edges_to2]).transpose(0,1)
+            data[node,'to','bus'].edge_index = torch.LongTensor([merged_df.index.tolist()+merged_df.index.tolist(),edges_to+edges_to2])
 
         if "bus" in merged_df.columns:
             edges_ = merged_df["bus"].tolist()
             merged_df.drop(columns=["bus"],inplace=True)
-            data['bus','to',node].edge_index = torch.Tensor([edges_, merged_df.index.tolist()]).transpose(0,1)
+            data['bus','to',node].edge_index = torch.LongTensor([edges_, merged_df.index.tolist()])
 
         dataframes[node] = merged_df
         edges[node] = [edges_, edges_from, edges_to, edges_to2]
@@ -103,4 +116,4 @@ def build_hetero_data(network, include_res=True, opf_as_y=True):
 
         print(node, len(getattr(network,node).columns), len(merged_df.columns))
 
-    return data, edges, dataframes
+    return data, edges, dataframes, scalers
