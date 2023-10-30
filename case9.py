6841e85ec1d24969c1a9407d9f86ef4dfe7b3d98
yamizi <salah.ghamizi@gmail.com>
2023-10-30 13:23:48 +0100

    Added Case9 Training

--- case9.py
+++ case9.py
@@ -4,8 +4,16 @@ from matplotlib import pyplot as plt
 import sys
 sys.path.append("../")
 
-from utils.pandapower import build_costs, build_hetero_data, PandaPowerDataset
+from utils.pandapower import PandaPowerDataset
 import pandapower as pp
+from torch_geometric.nn import to_hetero
+
+import torch_geometric.transforms as T
+from torch_geometric.loader import NeighborLoader, DataLoader
+import torch
+import torch.nn.functional as F
+from utils.base_gnn import GNN
+from utils.train import train_step
 
 network = pp.networks.case9()
 
@@ -13,9 +21,27 @@ network = pp.networks.case9()
 graph = PandaPowerDataset(network)
 
 pp.runopp(network, delta=1e-16)
-graph_y = PandaPowerDataset(network,include_res=False,opf_as_y=True)
-graph_opp_y = PandaPowerDataset(network,include_res=True,opf_as_y=True)
-graph_opp = PandaPowerDataset(network,include_res=True,opf_as_y=False)
+transforms = [T.ToUndirected(merge=True)]
+graph_y = PandaPowerDataset(network,include_res=False,opf_as_y=True, preprocess='metapath2vec',
+                    transform=T.Compose(transforms))
+graph_opp_y = PandaPowerDataset(network,include_res=True,opf_as_y=True, preprocess='metapath2vec',
+                    transform=T.Compose(transforms))
+graph_opp = PandaPowerDataset(network,include_res=True,opf_as_y=False, preprocess='metapath2vec',
+                    transform=T.Compose(transforms))
 
-print()
 
+from pandapower.plotting import simple_plot
+#simple_plot(network, plot_loads=True)
+
+data = graph_y[0]
+print(data.has_isolated_nodes(),data.has_self_loops(),data.is_undirected())
+
+model = GNN(hidden_channels=64, out_channels=graph_y.num_outputs)
+model = to_hetero(model, data.metadata(), aggr='sum')
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+for a in range(1,21):
+    print("multistep train epoch",a)
+    out, loss = train_step(model, optimizer,data,None,"gen",torch.nn.L1Loss())
+    print(loss)
---
+++ utils/train.py
@@ -0,0 +1,21 @@
+import torch.nn.functional as F
+
+def train_step(model, optimizer, data, mask_node="paper", feature_node="paper", loss_f=None):
+    model.train()
+    optimizer.zero_grad()
+    if loss_f is None:
+        loss_f = F.cross_entropy
+    out = model(data.x_dict, data.edge_index_dict)
+    
+    label = data[feature_node].y
+    output = out[feature_node]
+    if mask_node is not None:
+        mask = data[mask_node].train_mask
+        label = label[mask]
+        output = output[mask]
+
+    print(label, output)
+    loss = loss_f(label, output)
+    loss.backward()
+    optimizer.step()
+    return out, float(loss)
