b89acd039e2901962d58ac72bfe71054102e6c1d
yamizi <salah.ghamizi@gmail.com>
2023-11-05 11:49:45 +0100

    update eval for homoGNN

--- utils/train.py
+++ utils/train.py
@@ -63,7 +63,7 @@ def train_opf(model,train_loader, val_loader, max_epochs=200, y_nodes=["gen","ex
         out_all = []
         with torch.no_grad():
             for batch in val_loader:
-                last_out, loss, losses, b_losses = eval_step(model, batch,None,y_nodes,loss_fn)
+                last_out, loss, losses, b_losses = eval_step(model, batch,None,y_nodes,loss_fn, homo)
                 val_loss += loss
                 boundary_loss+= np.concatenate(b_losses,0).max()
                 val_losses_all.append(losses)
@@ -88,7 +88,8 @@ def train_opf(model,train_loader, val_loader, max_epochs=200, y_nodes=["gen","ex
 
     return train_losses, val_losses, val_losses_gen, val_losses_ext_grid, (out_all, val_losses_all), boundary_train_losses, boundary_val_losses, learning_rate
 
-def train_step(model, optimizer, data, mask_node="paper", feature_node="paper", loss_f=None, homo=True):
+def train_step(model, optimizer, data, mask_node="paper", feature_node="paper", loss_f=None, homo=True, 
+               use_boundary_loss=True):
     model.train()
     optimizer.zero_grad()
     if loss_f is None:
@@ -115,10 +116,13 @@ def train_step(model, optimizer, data, mask_node="paper", feature_node="paper", 
                 output = output[mask_node[i]]
 
             loss_label = loss_f(label, output)
-            loss_boundary = boundary_loss(data[node].boundaries, output)
-            loss_node = torch.cat([loss_label,loss_boundary.unsqueeze(1)],1)
             losses.append(loss_label.cpu().detach().numpy())
-            boundary_losses.append(loss_boundary.cpu().detach().numpy())
+            if use_boundary_loss:
+                loss_boundary = boundary_loss(data[node].boundaries, output)
+                loss_node = torch.cat([loss_label,loss_boundary.unsqueeze(1)],1)
+                boundary_losses.append(loss_boundary.cpu().detach().numpy())
+            else:
+                loss_node = loss_label
             loss += loss_node.mean()
     else:
         out =  model(data.x, data.edge_index)
@@ -143,7 +147,7 @@ def train_step(model, optimizer, data, mask_node="paper", feature_node="paper", 
     return out, float(loss), losses, boundary_losses
 
 
-def eval_step(model, data, mask_node="paper", feature_node="paper", loss_f=None):
+def eval_step(model, data, mask_node="paper", feature_node="paper", loss_f=None, homo=True):
     model.eval()
     if loss_f is None:
         loss_f = F.cross_entropy
@@ -152,23 +156,40 @@ def eval_step(model, data, mask_node="paper", feature_node="paper", loss_f=None)
         feature_node = [feature_node]
     if isinstance(mask_node,str):
         mask_node = [mask_node]
-
-    out = model(data.x_dict, data.edge_index_dict)
     loss = 0
     losses = []
     boundary_losses = []
-    for i, node in enumerate(feature_node):    
-        label = data[node].y
+
+    if homo:
+        out = model(data.x_dict, data.edge_index_dict)
+        
+        for i, node in enumerate(feature_node):    
+            label = data[node].y
+            output = out[node]
+            if mask_node is not None:
+                mask = data[mask_node[i]].train_mask
+                label = label[mask_node[i]]
+                output = output[mask_node[i]]
+
+            loss_node = loss_f(label, output)
+            loss_boundary = boundary_loss(data[node].boundaries, output)
+            losses.append(loss_node.cpu().detach().numpy())
+            boundary_losses.append(loss_boundary.cpu().detach().numpy())
+            loss += loss_node.mean()
+    else:
+        out =  model(data.x, data.edge_index)
+        mask = ~torch.isnan(data.y).any(1)
+        label = data.y[mask]
+        output = out[mask]
         output = out[node]
         if mask_node is not None:
             mask = data[mask_node[i]].train_mask
             label = label[mask_node[i]]
             output = output[mask_node[i]]
-
         loss_node = loss_f(label, output)
-        loss_boundary = boundary_loss(data[node].boundaries, output)
+        #loss_boundary = boundary_loss(data[node].boundaries, output)
         losses.append(loss_node.cpu().detach().numpy())
-        boundary_losses.append(loss_boundary.cpu().detach().numpy())
+        #boundary_losses.append(loss_boundary.cpu().detach().numpy())
         loss += loss_node.mean()
 
     return out, float(loss),  losses, boundary_losses
