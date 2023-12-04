eda4244f309ec25b1c25f16063b83a31bc59fb18
Salah GHAMIZI <salah.ghamizi@gmail.com>
2023-12-04 11:19:09 +0100

    Added support for dataset reuse from previous uniqueid

--- experiments/test_performances.py
+++ experiments/test_performances.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append(".")
 from utils.io import get_parser
-
+import hashlib
 from utils.logging import init_comet
 from runs.diff_distribution import run_case
 
@@ -16,11 +16,14 @@ def run(mutations = ["cost", "load_relative"],cases = ["case9","case14","case30"
             experiment = init_comet({"case":case, "mutation":mutation},project_name)
             training_case=[[case,nb_train,0.7,[mutation]]]
             validation_case=[case,nb_val,0.7,[mutation]]
-            path = "./output/test_perf/"+mutation+"/"+case
+            path = "./output/test_perf"
+            hash_path = f"{training_case}_{validation_case}"
+            hash_path = hashlib.md5(hash_path.encode()).hexdigest()
+
             run_case(training_cases=training_case,validation_case=validation_case, plot=False,
                      title="Test performance on "+mutation, save_path=path, dataset_type=dataset_type,
-                     experiment=experiment,train_batch_size=128,val_batch_size=256,scale=scale,
-                     device=device, opf=opf,use_ray=use_ray)
+                     experiment=experiment,train_batch_size=256,val_batch_size=512,scale=scale,
+                     device=device, opf=opf,use_ray=use_ray,uniqueid=hash_path)
 
 
 if __name__ == "__main__":
--- runs/diff_distribution.py
+++ runs/diff_distribution.py
@@ -1,5 +1,6 @@
+import os.path
 import sys
-
+import hashlib
 sys.path.append(".")
 import uuid
 from matplotlib import pyplot as plt
@@ -15,19 +16,16 @@ import torch
 from utils.train import train_opf
 from utils.plot import plot_losses, plot_results
 import json
+import pickle
 
 
 def run_case(training_cases=[["case9", 64, 0.7, ["cost", "load"]]], experiment=None,
              validation_case=["case9", 64, 0.7, ["cost", "load"]], plot=True,
              save_path="./output", title="", dataset_type="y_OPF", scale=False,
-             max_epochs=500, y_nodes=["gen", "ext_grid","bus"], train_batch_size=5, val_batch_size=5,
-             device="cuda", filter=True, opf=2,use_ray=True):
-    uniqueid = uuid.uuid4()
-    if experiment is not None:
-        experiment.log_parameters({"uniqueid": uniqueid, "max_epochs": max_epochs, "dataset_type": dataset_type,
-                                   "scale": scale, "type": "hetero", "opf": opf,"use_ray":use_ray,
-                                   "save_path": save_path, "title": title, "y_nodes": y_nodes, "plot": plot,
-                                   "device": device})
+             max_epochs=500, y_nodes=["gen", "ext_grid", "bus"], train_batch_size=5, val_batch_size=5,
+             device="cuda", filter=True, opf=2, use_ray=True, uniqueid=""):
+    if not uniqueid:
+        uniqueid = uuid.uuid4()
 
     if torch.cuda.is_available() and "cuda" in device:
         device = device
@@ -36,38 +34,63 @@ def run_case(training_cases=[["case9", 64, 0.7, ["cost", "load"]]], experiment=N
         device = "cpu"
         print("running with cpu backend")
 
-    common_params = {"dataset_type": dataset_type, "save_dataframes": save_path, "experiment": experiment,
-                     "scale": scale, "device": device, "opf": opf,"use_ray":use_ray}
+    pickle_file = f"{save_path}/{case}_{uniqueid}.pkl"
 
-    val_case_name, nb_graphs, mutation_rate, mutations = validation_case
-    val_graphs, valid_networks, _, _ = build_dataset(val_case_name, nbsamples=nb_graphs,
-                                                     mutation_rate=mutation_rate, uniqueid="{}/val".format(uniqueid),
-                                                     mutations=mutations, **common_params)
-    print("Correct validation graphs {}/{}".format(len(val_graphs), nb_graphs))
-    experiment.log_metric("nb_valid_graphs", len(val_graphs))
+    if experiment is not None:
+        experiment.log_parameters({"uniqueid": uniqueid, "max_epochs": max_epochs, "dataset_type": dataset_type,
+                                   "scale": scale, "type": "hetero", "opf": opf, "use_ray": use_ray,
+                                   "save_path": save_path, "title": title, "y_nodes": y_nodes, "plot": plot,
+                                   "device": device, "train_batch_size": train_batch_size,
+                                   "val_batch_size": val_batch_size, "pickle_file":pickle_file})
 
-    train_graphs = []
-    train_networks = {"mutants": []}
-    nb_graphs = 0
-    train_case_name = ""
-    for training_case in training_cases:
-        train_case_name, nb_graph, mutation_rate, mutations = training_case
+    if(os.path.exists(pickle_file)):
+        loaded = pickle.load(open(pickle_file, "rb"))
+        train_graphs, train_networks, val_graphs, valid_networks = loaded.get("train_graphs"), loaded.get("train_networks"), loaded.get("val_graphs"), loaded.get("valid_networks")
+        train_case_name, nb_graph, mutation_rate, mutations = training_cases[0]
+        val_case_name, nb_graphs, mutation_rate, mutations = validation_case
 
-        train_graph, train_network, _, _ = build_dataset(train_case_name, nbsamples=nb_graph,
+    else:
+
+        common_params = {"dataset_type": dataset_type, "save_dataframes": save_path, "experiment": experiment,
+                         "scale": scale, "device": device, "opf": opf, "use_ray": use_ray}
+
+        val_case_name, nb_graphs, mutation_rate, mutations = validation_case
+        val_graphs, valid_networks, _, _ = build_dataset(val_case_name, nbsamples=nb_graphs,
                                                          mutation_rate=mutation_rate,
-                                                         uniqueid="{}/train".format(uniqueid),
+                                                         uniqueid="{}/val".format(uniqueid),
                                                          mutations=mutations, **common_params)
+        print("Correct validation graphs {}/{}".format(len(val_graphs), nb_graphs))
+        experiment.log_metric("nb_valid_graphs", len(val_graphs))
 
-        train_graphs += train_graph
-        train_networks["mutants"] += train_network["mutants"]
-        nb_graphs += nb_graph
+        train_graphs = []
+        train_networks = {"original": [],"mutants": []}
+        nb_graphs = 0
+        train_case_name = ""
+        for training_case in training_cases:
+            train_case_name, nb_graph, mutation_rate, mutations = training_case
 
-    if filter:
-        train_graphs, train_networks, val_graphs, valid_networks = clear_duplicates(train_graphs, train_networks,
-                                                                                    val_graphs, valid_networks)
+            train_graph, train_network, _, _ = build_dataset(train_case_name, nbsamples=nb_graph,
+                                                             mutation_rate=mutation_rate,
+                                                             uniqueid="{}/train".format(uniqueid),
+                                                             mutations=mutations, **common_params)
 
-    print("Correct training graphs {}/{}".format(len(train_graphs), nb_graphs))
-    experiment.log_metric("nb_train_graphs", len(train_graphs))
+            train_graphs += train_graph
+            train_networks["mutants"] += train_network["mutants"]
+            train_networks["original"] += train_network["original"]
+            nb_graphs += nb_graph
+
+        if filter:
+            train_graphs, train_networks, val_graphs, valid_networks = clear_duplicates(train_graphs, train_networks,
+                                                                                        val_graphs, valid_networks)
+
+        print("Correct training graphs {}/{}".format(len(train_graphs), nb_graphs))
+        experiment.log_metric("nb_train_graphs", len(train_graphs))
+
+        with(open(pickle_file, "ab") as f):
+            pickle.dump({"train_graphs": train_graphs, "train_networks": train_networks, "val_graphs": val_graphs,
+                         "valid_networks": valid_networks}, f)
+
+
 
     train_loader = DataLoader([g[0].to(device) for g in train_graphs], batch_size=train_batch_size)
     val_loader = DataLoader([g[0].to(device) for g in val_graphs], batch_size=val_batch_size)
@@ -80,7 +103,7 @@ def run_case(training_cases=[["case9", 64, 0.7, ["cost", "load"]]], experiment=N
     model = GNN(initial_channels=128, hidden_channels=64, nb_hidden_layers=3, out_channels=graph_y.num_outputs)
     model = to_hetero(model, data.metadata(), aggr='sum').to(device)
 
-    print("model device",next(model.parameters()).device)
+    print("model device", next(model.parameters()).device)
 
     train_losses, val_losses, val_losses_nodes, last_out, b_train_losses, b_val_losses, lr = train_opf(
         model, train_loader, val_loader, max_epochs=max_epochs, y_nodes=y_nodes, device=device)
@@ -97,12 +120,13 @@ def run_case(training_cases=[["case9", 64, 0.7, ["cost", "load"]]], experiment=N
                 "val_losses": val_losses,
                 "b_train_losses": b_train_losses, "b_val_losses": b_val_losses, "learning_rate": lr,
                 "val_losses_gen": val_losses_gen, "val_losses_ext_grid": val_losses_ext_grid,
-                "val_losses_bus":val_losses_bus}
+                "val_losses_bus": val_losses_bus}
 
     with open(save_path + "/losses.json", "w") as outfile:
         json.dump(log_dict, outfile)
 
     if experiment is not None:
+        experiment.log_asset(pickle_file)
         log_dict_series(log_dict, experiment)
         log_opf(valid_networks, val_graphs, last_out, y_nodes, experiment)
 
@@ -121,10 +145,12 @@ if __name__ == "__main__":
     opf = 1
 
     experiment = init_comet({"case": case, "mutation": mutation})
+    hash_path = f"{training_case}_{validation_case}"
+    hash_path = hashlib.md5(hash_path.encode()).hexdigest()
     run_case(training_cases=training_case, validation_case=validation_case, val_batch_size=50, train_batch_size=32,
-             title="generalization load_relative", save_path=f"./output/case{training_case}_{validation_case}",
+             title="generalization load_relative", save_path=f"./output",
              max_epochs=max_epochs, experiment=experiment, dataset_type="y_no_OPF",
-             scale=False, filter=True, opf=opf,use_ray=False)
+             scale=False, filter=True, opf=opf, use_ray=False, uniqueid=hash_path)
     plt.show()
     exit()
 
