d7d9269ae21a3d0cde9b25eaf60e56670fddda9e
yamizi <salah.ghamizi@gmail.com>
2024-04-03 11:03:11 +0200

    Refactored logging with common method

--- utils/logging.py
+++ utils/logging.py
@@ -57,6 +57,24 @@ def log_dict_series(dic, experiment, limit=0):
             counter = 0
 
 
+def build_log_opf(dic, label, node, outputs, output_index, ground_truth, gt_index, delta=1e-10):
+    dic = {**dic,
+           f"{label}_pred_" + node: outputs[:, output_index].cpu().numpy(),
+           f"{label}_true_" + node: ground_truth[:, gt_index].cpu().numpy(),
+           }
+
+    SE = (outputs[:, output_index].cpu().numpy() - ground_truth[:, gt_index].cpu().numpy()) ** 2
+    relativeSE = SE / (ground_truth[:, gt_index].cpu().numpy() ** 2 + delta)
+
+    AE = (outputs[:, output_index].cpu().numpy() - ground_truth[:, gt_index].cpu().numpy())
+    relativeAE = AE / (ground_truth[:, gt_index].cpu().numpy() + delta)
+
+    dic = {**dic, f"SE_{label}_" + node: SE, f"relativeSE_{label}_" + node: relativeSE, f"AE_{label}_" + node: AE,
+           f"relativeAE_{label}_" + node: relativeAE}
+
+    return dic
+
+
 def log_opf(networks, val_graphs, outputs, y_nodes, experiment, hetero=True):
     (out_all, val_losses_all) = outputs
     if hetero:
@@ -77,64 +95,24 @@ def log_opf(networks, val_graphs, outputs, y_nodes, experiment, hetero=True):
     dic = {}
     for node, outputs in output_nodes.items():
         ground_truth = labels[node]
+        if node=="gen":
+            dic = build_log_opf(dic, "P", node, outputs, 0, ground_truth, 0)
+            dic = build_log_opf(dic, "Q", node, outputs, 1, ground_truth, 1)
 
-        if node in ["gen", "ext_grid"]:
-            dic = {**dic,
-                   "P_pred_" + node: outputs[:, 0].cpu().numpy(), "P_true_" + node: ground_truth[:, 0].cpu().numpy(),
-                   "Q_pred_" + node: outputs[:, 1].cpu().numpy(), "Q_true_" + node: ground_truth[:, 1].cpu().numpy()}
+        elif node=="ext_grid":
+            dic = build_log_opf(dic, "P", node, outputs, 2, ground_truth, 0)
+            dic = build_log_opf(dic, "Q", node, outputs, 3, ground_truth, 1)
 
-            SE_P = (outputs[:, 0].cpu().numpy() - ground_truth[:, 0].cpu().numpy()) ** 2
-            relativeSE_P = SE_P / (ground_truth[:, 0].cpu().numpy() ** 2 + delta)
+        elif node =="bus":
+            dic = build_log_opf(dic, "Vm", node, outputs, 4, ground_truth, 0)
+            dic = build_log_opf(dic, "Va", node, outputs, 5, ground_truth, 1)
 
-            SE_Q = (outputs[:, 1].cpu().numpy() - ground_truth[:, 1].cpu().numpy()) ** 2
-            relativeSE_Q = SE_P / (ground_truth[:, 1].cpu().numpy() ** 2 + delta)
+        if node == "line":
+            dic = build_log_opf(dic, "pl_mw", node, outputs, 6, ground_truth, 0)
+            dic = build_log_opf(dic, "SE_ql", node, outputs, 7, ground_truth, 1)
 
-            dic = {**dic, "SE_P_" + node: SE_P, "relativeSE_P_" + node: relativeSE_P, "SE_Q_" + node: SE_Q,
-                   "relativeSE_Q_" + node: relativeSE_Q}
-        if node in ["bus"]:
-            dic = {"Vm_pred_" + node: outputs[:, 2].cpu().numpy(), "Vm_true_" + node: ground_truth[:, 0].cpu().numpy(),
-                   "Va_pred_" + node: outputs[:, 3].cpu().numpy(), "Va_true_" + node: ground_truth[:, 1].cpu().numpy(),
-                   **dic}
-
-            SE_Vm = (outputs[:, 2].cpu().numpy() - ground_truth[:, 0].cpu().numpy()) ** 2
-            relativeSE_Vm = SE_Vm / (ground_truth[:, 0].cpu().numpy() ** 2 + delta)
-
-            SE_Va = (outputs[:, 3].cpu().numpy() - ground_truth[:, 1].cpu().numpy()) ** 2
-            relativeSE_Va = SE_Va / (ground_truth[:, 1].cpu().numpy() ** 2 + delta)
-
-            dic = {**dic, "SE_Vm_" + node: SE_Vm, "relativeSE_Vm_" + node: relativeSE_Vm, "SE_Va_" + node: SE_Va,
-                   "relativeSE_Va_" + node: relativeSE_Va}
-
-        if node in ["line"]:
-            dic = {"pl_mw_pred_" + node: outputs[:, 4].cpu().numpy(),
-                   "pl_mw_true_" + node: ground_truth[:, 0].cpu().numpy(),
-                   "Va_pred_" + node: outputs[:, 5].cpu().numpy(), "Va_true_" + node: ground_truth[:, 1].cpu().numpy(),
-                   **dic}
-
-            SE_pl_mw = (outputs[:, 4].cpu().numpy() - ground_truth[:, 0].cpu().numpy()) ** 2
-            relativeSE_pl_mw = SE_pl_mw / (ground_truth[:, 0].cpu().numpy() ** 2 + delta)
-
-            SE_ql_mvar = (outputs[:, 5].cpu().numpy() - ground_truth[:, 1].cpu().numpy()) ** 2
-            relativeSE_ql_mvar = SE_ql_mvar / (ground_truth[:, 1].cpu().numpy() ** 2 + delta)
-
-            dic = {**dic, "SE_pl_mw_" + node: SE_pl_mw, "relativeSE_pl_mw_" + node: relativeSE_pl_mw,
-                   "SE_ql_mvar_" + node: SE_ql_mvar,
-                   "relativeSE_ql_mvar_" + node: relativeSE_ql_mvar}
-
-            dic = {"i_from_ka_pred_" + node: outputs[:, 6].cpu().numpy(),
-                   "i_from_ka_true_" + node: ground_truth[:, 2].cpu().numpy(),
-                   "Va_pred_" + node: outputs[:, 7].cpu().numpy(), "Va_true_" + node: ground_truth[:, 3].cpu().numpy(),
-                   **dic}
-
-            SE_i_from_ka = (outputs[:, 6].cpu().numpy() - ground_truth[:, 2].cpu().numpy()) ** 2
-            relativeSE_i_from_ka = SE_i_from_ka / (ground_truth[:, 2].cpu().numpy() ** 2 + delta)
-
-            SE_i_to_ka = (outputs[:, 7].cpu().numpy() - ground_truth[:, 3].cpu().numpy()) ** 2
-            relativeSE_i_to_ka = SE_i_to_ka / (ground_truth[:, 3].cpu().numpy() ** 2 + delta)
-
-            dic = {**dic, "SE_i_to_ka_" + node: SE_i_to_ka, "relativeSE_i_to_ka_" + node: relativeSE_i_to_ka,
-                   "SE_i_from_ka_" + node: SE_i_from_ka,
-                   "relativeSE_i_from_ka_" + node: relativeSE_i_from_ka}
+            dic = build_log_opf(dic, "i_from_ka", node, outputs, 7, ground_truth, 2)
+            dic = build_log_opf(dic, "i_to_ka", node, outputs, 8, ground_truth, 3)
 
     if experiment:
         log_dict_series(dic, experiment)
